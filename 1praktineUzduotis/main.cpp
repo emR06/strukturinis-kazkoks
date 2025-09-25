402b665c251e0fd1742db291e16f425082349a00
emR06 <emriab06@gmail.com>
2025-09-25 08:47:35 +0300

    beliko pagrazinti koda

--- 1praktineUzduotis/main.cpp
+++ 1praktineUzduotis/main.cpp
@@ -2,6 +2,7 @@
 
 void pirkti(int pasirink);
 void parduoti(int pasirink);
+void palygint(int pasirink);
 
 using namespace std;
 
@@ -14,22 +15,22 @@ int main() {
     cout<<"3: Galimybe parduoti valiuta."<<endl;
     cout<<"Jusu pasirinkimas: ";
     cin>>pasirink;
-    cout<<endl<<"Jus pasirinkote: ";
+    cout<<"Jus pasirinkote: ";
 
     switch(pasirink) {
-        case 1: cout<<"valiutos kurso palyginimas su kita valiuta."; break;
+        case 1: cout<<"valiutos kurso palyginimas su kita valiuta."; palygint(pasirink); break;
         case 2: cout<<"galimybe isigyti valiutos."; pirkti(pasirink); break;
         case 3: cout<<"galimybe parduoti valiuta."; parduoti(pasirink); break;
+        default: cout<<"error"; break;
     }
     return 0;
 }
 
-void pirkti(int pasirink){
+void palygint(int pasirink) {
     int n;
-    float GBP_Pirkti=0.8600;
-    float USD_Pirkti=1.1460;
-    float INR_Pirkti=101.3862;
-
+    float GBP_Bendras=0.8729;
+    float USD_Bendras=1.1793;
+    float INR_Bendras=104.6918;
     cout<<endl<<"///////////////"<<endl;
     cout<<"Prasome pasirinkti valiuta:"<<endl;
     cout<<"1: GBP"<<endl;
@@ -43,6 +44,47 @@ void pirkti(int pasirink){
         case 1: cout<<"GBP"<<endl; break;
         case 2: cout<<"USD"<<endl; break;
         case 3: cout<<"INR"<<endl; break;
+        default: cout<<"error"; break;
+    }
+
+    cout<<endl<<"1 EUR = ";
+    switch(pasirink) {
+        case 1: cout<<GBP_Bendras<<" GBP."; break;
+        case 2: cout<<USD_Bendras<<" USD."; break;
+        case 3: cout<<INR_Bendras<<" INR."; break;
+        default: cout<<"error"; break;
+    }
+
+    cout<<endl<<"Prasome irasyti EUR kieki."<<endl;
+    cin>>n;
+    cout<<endl<<n<<" EUR = ";
+    switch(pasirink) {
+        case 1: cout<<GBP_Bendras*n<<" GBP."; break;
+        case 2: cout<<USD_Bendras*n<<" USD."; break;
+        case 3: cout<<INR_Bendras*n<<" INR."; break;
+        default: cout<<"error"; break;
+    }
+}
+
+void pirkti(int pasirink){
+    int n;
+    float GBP_Pirkti=0.8600;
+    float USD_Pirkti=1.1460;
+    float INR_Pirkti=101.3862;
+    cout<<endl<<"///////////////"<<endl;
+    cout<<"Prasome pasirinkti valiuta:"<<endl;
+    cout<<"1: GBP"<<endl;
+    cout<<"2: USD"<<endl;
+    cout<<"3: INR"<<endl;
+    cout<<"Jusu pasirinkimas:";
+    cin>>pasirink;
+    cout<<endl<<"Jus pasirinkote: ";
+
+    switch(pasirink) {
+        case 1: cout<<"GBP"<<endl; break;
+        case 2: cout<<"USD"<<endl; break;
+        case 3: cout<<"INR"<<endl; break;
+        default: cout<<"error"; break;
     }
 
     cout<<endl<<"Uz 1 EUR galima nupirkti ";
@@ -50,6 +92,7 @@ void pirkti(int pasirink){
         case 1: cout<<GBP_Pirkti<<" GBP."; break;
         case 2: cout<<USD_Pirkti<<" USD."; break;
         case 3: cout<<INR_Pirkti<<" INR."; break;
+        default: cout<<"error"; break;
     }
 
     cout<<endl<<"Prasome irasyti uz kiek EUR pirksite."<<endl;
@@ -59,6 +102,7 @@ void pirkti(int pasirink){
         case 1: cout<<GBP_Pirkti*n<<" GBP."; break;
         case 2: cout<<USD_Pirkti*n<<" USD."; break;
         case 3: cout<<INR_Pirkti*n<<" INR."; break;
+        default: cout<<"error"; break;
     }
 
 }
@@ -68,7 +112,6 @@ void parduoti(int pasirink) {
     float GBP_Parduoti=0.9220;
     float USD_Parduoti=1.2340;
     float INR_Parduoti=107.8546;
-
     cout<<endl<<"///////////////"<<endl;
     cout<<"Prasome pasirinkti valiuta:"<<endl;
     cout<<"1: GBP"<<endl;
@@ -82,6 +125,7 @@ void parduoti(int pasirink) {
         case 1: cout<<"GBP"<<endl; break;
         case 2: cout<<"USD"<<endl; break;
         case 3: cout<<"INR"<<endl; break;
+        default: cout<<"error"; break;
     }
 
     cout<<endl<<"Pardavus 1 ";
@@ -89,6 +133,7 @@ void parduoti(int pasirink) {
         case 1: cout<<"GBP gausite "<<GBP_Parduoti<<" EUR."; break;
         case 2: cout<<"USD gausite "<<USD_Parduoti<<" EUR."; break;
         case 3: cout<<"INR gausite "<<INR_Parduoti<<" EUR."; break;
+        default: cout<<"error"; break;
     }
 
     cout<<endl<<"Prasome irasyti kiek parduosite."<<endl;
@@ -98,5 +143,6 @@ void parduoti(int pasirink) {
         case 1: cout<<" GBP gausite "<<GBP_Parduoti*n<<" EUR."; break;
         case 2: cout<<" USD gausite "<<USD_Parduoti*n<<" EUR."; break;
         case 3: cout<<" INR gausite "<<INR_Parduoti*n<<" EUR."; break;
+        default: cout<<"error"; break;
     }
 }
