0d1f3a81b8cdd1a6c58d80036c6a21cae697f2e3
emR06 <emriab06@gmail.com>
2026-01-08 22:45:03 +0200

    pataisyta, checkpoint

--- 5praktineuzduotis/mainn.cpp
+++ 5praktineuzduotis/mainn.cpp
@@ -13,24 +13,38 @@ struct Student{
     float percent;
 };
 
-void sk(vector<Student>* students, int* Ats);
-void apdoroti(vector<Student>* students, int* Ats);
-void sp(vector<Student>* students);
+struct Question{
+    int right=0;
+    int wrong=0;
+    int und=0;
+    float percent;
+};
+
+void sk(vector<Student> &students, char* Ats);
+void apdoroti(vector<Student> &students, char* Ats);
+void sp(vector<Student> students);
+void stats(vector<Student> students);
+int maximum(vector<Student> students);
+int minimum(vector<Student> students);
+float vidurkSc(vector<Student> students);
+float vidurkPr(vector<Student> students);
+int counting(vector<Student> students,int num);
+void statsKl(vector<Student> students,char* Ats);
+
+
 
 int main ()
 {
     vector<Student> students;
-    int *Ats;
-    Ats = new int[20];
+    char Ats[20];
     sk(students,Ats);
     apdoroti(students,Ats);
 
     //menu
-    cout<<"Duomenys apdoroti."<<endl
+    cout<<"Duomenys apdoroti."<<endl;
     int p=0;
     while(p!=5){
         cout<<"Pasirinkite funkcija:"<<endl;
-        cout<<endl;
         cout<<"1) Rezultatu isvedimas i faila."<<endl;
         cout<<"2) Klases statistika."<<endl;
         cout<<"3) Klausimu statistika."<<endl;
@@ -43,17 +57,23 @@ int main ()
         {
         case 1:
             {
-                cout<<endl<<"Spausdinima..."
+                cout<<"Spausdinima...";
                 sp(students);
-                cout<<endl<<"Baigta."<<endl;
+                cout<<endl<<"Baigta."<<endl<<endl;
                 break;
             }
         case 2:
             {
+                cout<<"Klases statistika."<<endl;
+                stats(students);
+                cout<<"Baigta."<<endl<<endl;
                 break;
             }
         case 3:
             {
+                cout<<"Klausimu statistika."<<endl;
+                statsKl(students,Ats);
+                cout<<"Baigta."<<endl<<endl;
                 break;
             }
         case 4:
@@ -62,6 +82,7 @@ int main ()
             }
         case 5:
             {
+
                 cout<<endl<<"Viso gero.";
             }
         }
@@ -70,7 +91,7 @@ int main ()
 return 0;
 }
 
-void sk(vector<Student>* students, int* Ats)
+void sk(vector<Student> &students, char* Ats)
 {
     ifstream fd("testData.txt");
     //atsakymu skaitymas
@@ -80,23 +101,22 @@ void sk(vector<Student>* students, int* Ats)
     }
 
     //studentu info apdorojimas
-    students.push_back(Student()); //sukuriame nauja vektoriaus elementa
     int n=0;
     while(!fd.eof())
     {
-
+        students.push_back(Student());
         fd>>students[n].id;
         for(int i=0; i<20; i++)
         {
             fd>>students[n].answer[i];
         }
-        students.push_back(Student());
         n++;
     }
+    students.pop_back();
     fd.close();
 }
 
-void apdoroti(vector<Student>* students, int* Ats)
+void apdoroti(vector<Student> &students, char* Ats)
 {
     //pereinam per visus studentus
     for(int i=0; i<students.size(); i++)
@@ -109,33 +129,126 @@ void apdoroti(vector<Student>* students, int* Ats)
         }
         //suskaiciuojamas studento procentas
         students[i].percent = (students[i].score/40.0)*100;
-
         //priskyriame pazymi (visai negrazus sprendimas, bet tebunie)
-        if(students[i].percent>94){students[i].grade == 10;}
-        if else(students[i].percent>84){students[i].grade==9;}
-        if else(students[i].percent>74){students[i].grade==8;}
-        if else(students[i].percent>64){students[i].grade==7;}
-        if else(students[i].percent>54){students[i].grade==6;}
-        if else(students[i].percent>49){students[i].grade==5;}
-        if else(students[i].percent>34){students[i].grade==4;}
-        if else(students[i].percent>24){students[i].grade==3;}
-        if else(students[i].percent>14){students[i].grade==2;}
-        else{students[i].grade==1;}
+        if(students[i].percent>94){students[i].grade = 10;}
+        else if(students[i].percent>84){students[i].grade=9;}
+        else if(students[i].percent>74){students[i].grade=8;}
+        else if(students[i].percent>64){students[i].grade=7;}
+        else if(students[i].percent>54){students[i].grade=6;}
+        else if(students[i].percent>49){students[i].grade=5;}
+        else if(students[i].percent>34){students[i].grade=4;}
+        else if(students[i].percent>24){students[i].grade=3;}
+        else if(students[i].percent>14){students[i].grade=2;}
+        else{students[i].grade=1;}
     }
 }
 
 //prasideda kitos funkcijos
 
-void sp(vector<Student>* students)
+void sp(vector<Student> students)
 {
     ofstream fr("results.txt");
 
     for(int i=0; i<students.size(); i++)
     {
-        fr<<students[i].id<<" "<<students[i].score<<" "<<students[i].procent<<" "<<students[i].grade<<endl;
+        fr<<students[i].id<<" "<<students[i].score<<" "<<students[i].percent<<"% "<<students[i].grade<<endl;
     }
 
     fr.close();
 }
 
+void stats(vector<Student> students)
+{
+    cout<<"Studentu skaicius: "<<students.size()<<endl;
+    cout<<"Didziausias balas: "<<maximum(students)<<endl;
+    cout<<"Maziausias balas: "<<minimum(students)<<endl;
+    cout<<"Vidutinis balas: "<<vidurkSc(students)<<endl;
+    cout<<"Vidutinis procentas: "<<vidurkPr(students)<<endl;
+    cout<<"Pasiskirstymas..."<<endl;
+    for(int i=1; i<=10; i++)
+    {
+        cout<<i<<"..."<<counting(students,i)<<endl;
+    }
+    cout<<endl;
+}
+
+int maximum(vector<Student> students)
+{
+    int m=-41;
+    for(int i=0; i<students.size(); i++)
+    {
+        if(students[i].score>m)
+        {
+            m=students[i].score;
+        }
+    }
+    return m;
+}
+
+int minimum(vector<Student> students)
+{
+    int m=41;
+    for(int i=0; i<students.size(); i++)
+    {
+        if(students[i].score<m)
+        {
+            m=students[i].score;
+        }
+    }
+    return m;
+}
+
+float vidurkSc(vector<Student> students)
+{
+    float sum=0;
+    for(int i=0; i<students.size(); i++)
+    {
+        sum+=students[i].score;
+    }
+    return sum/students.size();
+}
+
+float vidurkPr(vector<Student> students)
+{
+    float sum=0;
+    for(int i=0; i<students.size(); i++)
+    {
+        sum+=students[i].percent;
+    }
+    return sum/students.size();
+}
+
+int counting(vector<Student> students,int num)
+{
+    int sum=0;
+    for(int i=0; i<students.size(); i++)
+    {
+        if(students[i].grade == num)
+        {
+            sum++;
+        }
+    }
+    return sum;
+}
+
+void statsKl(vector<Student> students,char* Ats)
+{
+    Question qStats[20];
+    for(int i=0; i<students.size(); i++)
+    {
+        for(int j=0; j<20; j++)
+        {
+            if(students[i].answer[j] == Ats[j]){qStats[j].right++;}
+            else if(students[i].answer[j] == '-'){qStats[j].und++;}
+            else if(students[i].answer[j] != Ats[j]){qStats[j].wrong++;}
+        }
+    }
+
+    for(int i=1; i<=20; i++)
+    {
+        cout<<i<<"-o klausimo atsakymai:"<<endl;
+        cout<<"Teisingi: "<<qStats[i-1].right<<" | Neteisingi: "<<qStats[i-1].wrong<<" | Neatsakyti: "<<qStats[i-1].und<<endl;
+        cout<<"."<<endl;
+    }
+}
 
